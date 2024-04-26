039a4dedbcfb3abb4960731b1056d62d9115fa5c
Vera869 <verea8910@gmail.com>
2024-04-26 13:49:37 +0700

    +eslint config, sceletapp

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          A new project starting
-        </p>
+        <h1>Поиск юзеров</h1>
+        <br></br>
+        <svg>
+         <symbol id="icon-search"  viewBox="0 0 17 18"  xmlns="http://www.w3.org/2000/svg">
+          <path d="M11.9276 12.7748L15.37 17.0644" stroke="white" strokeLinecap="round"/>
+          <circle cx="8.48533" cy="8.48526" r="5.5" transform="rotate(-38.7469 8.48533 8.48526)" stroke="white"/>
+         </symbol>
+         </svg>
+        <input
+         type="search"
+         placeholder="Поиск"
+         name="search"></input>
+
+        <p>Здесь будет актуальный список пользователей</p>
       </header>
     </div>
   );
