f6030a9b81e2cb8d5a769823b8f74a2da2e60741
Vera869 <verea8910@gmail.com>
2024-04-26 20:48:05 +0700

    +Ctyled components, fonts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Поиск юзеров</h1>
-        <br></br>
-        <svg>
-         <symbol id="icon-search"  viewBox="0 0 17 18"  xmlns="http://www.w3.org/2000/svg">
-          <path d="M11.9276 12.7748L15.37 17.0644" stroke="white" strokeLinecap="round"/>
-          <circle cx="8.48533" cy="8.48526" r="5.5" transform="rotate(-38.7469 8.48533 8.48526)" stroke="white"/>
-         </symbol>
-         </svg>
-        <input
+      <h1>Поиск юзеров</h1>
+      <input
          type="search"
          placeholder="Поиск"
          name="search"></input>
-
+        <br></br>
         <p>Здесь будет актуальный список пользователей</p>
-      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { GlobalStyle } from './GlobalStyled';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <GlobalStyle />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
 );
